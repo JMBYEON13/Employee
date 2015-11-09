81a0e9f243685066fbc5370768e8b21d3d334a04
JMBYEON13 <26000km@gmail.com>
2015-11-09 16:53:29 +0900

    Member CRUD
    Create ==> insert into member
    Read  ==> select*from member
    Update ==> update member set
    Delete ==> delet from member

--- target/test-classes/sql/world.sql
+++ target/test-classes/sql/world.sql
@@ -1,7 +1,34 @@
 
-select * from city;
+drop table member;
+
+-- Member Table
+
+create table member(
+	id 				int AUTO_INCREMENT,
+	email 			varchar(255),
+	name 			varchar(255),
+	password 		varchar(255),
+	register_date	date,
+	constraint pk_id primary key (id)
+
+);
+--
+-- Member CRUD (Create, Read, Update, Delete)
+--
+-- create
+insert into member
+(email, name, password, register_date)
+values
+('xxx@yyy','홍길동','1234', '2015/11/11')
 
 
-select count(*) from city;
+--read
+select*from member;
 
-select count(*) from country;
+--update
+update member
+	set name='홍순이',
+		password = '9876';
+
+--Delete
+delete from member where id=6;
